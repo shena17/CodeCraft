171662774e882d18c319858b0ef1657a778c2199
Shenal <shenaldesilva17@gmail.com>
2024-03-12 14:29:20 +0530

    setup done

---
+++ BACKEND/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+//Setting up the server
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server up with port : " + PORT);
+});
+
+//Setting up the database connection
+const URL = process.env.MONGODB_URL;
+
+mongoose.set("strictQuery", true);
+mongoose.connect(URL, { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully!");
+});
