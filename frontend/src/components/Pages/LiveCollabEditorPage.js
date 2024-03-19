24ff0a04b9acff51b1b884b1517f566268a7a3c9
it21057410 <it21057410@my.sliit.lk>
2024-03-19 15:00:52 +0530

    react-avatar dependency added

--- frontend/src/components/Pages/LiveCollabEditorPage.js
+++ frontend/src/components/Pages/LiveCollabEditorPage.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from "../../images/logo.png";
+import '../../styles/liveEditor.css'
+import Client from '../LiveCollab/Client';
 
 export default function LiveCollabEditorPage() {
+
+  const [clients, setClients] = useState([
+    {socketId: 1, username: 'Thanuka'},
+    {socketId: 1, username: 'Sithmi'},
+  ]);
+    
+
   return (
-    <div>LiveCollabEditorPage</div>
+    <div className='livemainWrap'>
+
+      <div className='aside'>
+        <div className='asideInner'>
+          <div className='liveLogo'>
+
+            <div className='liveimage-Container'>
+                <img className="livelogoImage"
+                    src={logo} alt="codecraftlogo" 
+                />
+            </div>
+
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {
+              clients.map((client) => {
+                <Client 
+                      key={client.socketId} 
+                      username={client.username}
+                />
+              })      
+            }
+          </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className='liveeditorWrap'>
+          Editor is here
+        </div>
+
+    </div>
   )
 }
