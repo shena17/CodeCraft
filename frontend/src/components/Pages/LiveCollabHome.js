87e877fdc1f91db57473bcabb535c826a42ad284
it21057410 <it21057410@my.sliit.lk>
2024-03-19 12:23:52 +0530

    LiveCollab Home finished

--- frontend/src/components/Pages/LiveCollabHome.js
+++ frontend/src/components/Pages/LiveCollabHome.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import logo from "../../images/logo.png";
+import "../../styles/liveHome.css"
+import { colors } from '@mui/material';
 
 export default function LiveCollabHome() {
   return (
@@ -6,9 +9,43 @@ export default function LiveCollabHome() {
 
       <div className='formWrapper'>
 
-        <img src="../../images/logo.png" alt="codecraftlogo"/>
+      <center>
+      <div className='liveimage-Container'>
+            <img src={logo} alt="codecraftlogo" className="livehomeLogo"/>
+        </div>
+      </center>
+
+      <center>
+      <hr style={{ color: 'black', fontWeight: 'bold' }} />
+      <h4 className='mainLabel'> Paste invitation ROOM ID</h4>
+      </center>
+        
+        <div className='inputGroup'>
+              <input 
+                  type="text" 
+                  className='inputBox' 
+                  placeholder='ROOM ID'
+              />
+
+              <input 
+                  type="text" 
+                  className='inputBox' 
+                  placeholder='USERNAME'
+              />
+
+         <button className='btn joinBtn'>Join</button>
+
+         <span className='createInfo'>
+              If you don't have an invite then create &nbsp;
+              <a href="" className='createNewBtn'> 
+                  new ROOM
+              </a>
+          </span>
+
+        </div>
 
       </div>
+
 
     </div>
   )
