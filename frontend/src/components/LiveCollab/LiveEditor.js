d2e00beda944512baef8574d14e48f8496f09bab
it21057410 <it21057410@my.sliit.lk>
2024-03-20 16:02:13 +0530

    LiveEditor updated

--- frontend/src/components/LiveCollab/LiveEditor.js
+++ frontend/src/components/LiveCollab/LiveEditor.js
@@ -36,9 +36,39 @@ export default function LiveEditor() {
   }, [])
 
   return (
+
     <div className='editorCenter'>
         <textarea id="realtimeEditor" ref={textareaRef}></textarea>
-     </div>
+
+        <div className="row m-3">
+            <div className="col">
+                <div className='custom-textarea'>
+                    <label for = "inputforCode">Input</label>
+                    <textarea type="text" class="form-control" placeholder="First name" aria-label="First name"></textarea>
+                    <label for = "outputforCode">Output</label>
+                    <textarea type="text" class="form-control" placeholder="First name" aria-label="First name"></textarea>
+                </div>
+             </div>
+        </div>
+
+        <div className='chooseoption'>
+                    <div class="col-12 w-25">
+                        <label class="visually-hidden" for="inlineFormSelectPref">Preference</label>
+                        <select class="form-select" id="inlineFormSelectPref">
+                            <option selected>Choose...</option>
+                            <option value="Java">Java</option>
+                            <option value="Cpp">Cpp</option>
+                            <option value="Python">Python</option>
+                        </select>
+                    </div>
+
+    
+        </div>
+
+        <div className='successBtn'>
+                        <button type="button" class="btn btn-outline-success"><i class="bi bi-play">Run the Code</i></button>
+                    </div>     
+    </div>
          
    
   )
